457ee6a57c20410d10bdeef44acce0bf8a5f925d
upr2-dzhioev <a.dzhioev@gosnadzor.ru>
2018-01-11 15:01:18 +0300

    Create TabPresenter.

---
+++ modules/gwt/src/main/java/ru/dzhinn/echodata/gwt/client/application/ApplicationUiHandlers.java
@@ -0,0 +1,10 @@
+package ru.dzhinn.echodata.gwt.client.application;
+
+import com.gwtplatform.mvp.client.UiHandlers;
+
+/**
+ * Created by A.Dzhioev on 11.01.2018.
+ */
+public interface ApplicationUiHandlers extends UiHandlers {
+    void onLogoImageClick();
+}
